01c7a9ddbe2c05bf24fdf4f0badb08f879962688
HeyBroooo <mohitkumar4049a@gmail.com>
2024-03-15 02:02:19 +0530

    Both-page-Done

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button, Text } from "react-native";
-
-export default function AnimatedStyleUpdateExample(props) {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-     <Text>AnimatedStyleUpdateExample</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
